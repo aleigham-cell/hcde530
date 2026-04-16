b517b82cb614a49170881fdb845389be4c48216b
Aleigha Mattison <aleigham@uw.edu>
2026-04-15 20:05:05 -0700

    Added a dictionary for mapping non-numeric numbers up to 20

--- Week 3/week3_analysis_buggy.py
+++ Week 3/week3_analysis_buggy.py
@@ -4,6 +4,30 @@ import csv
 filename = "week3_survey_messy.csv"
 rows = []
 
+NUMBER_WORDS = {
+    "zero": 0,
+    "one": 1,
+    "two": 2,
+    "three": 3,
+    "four": 4,
+    "five": 5,
+    "six": 6,
+    "seven": 7,
+    "eight": 8,
+    "nine": 9,
+    "ten": 10,
+    "eleven": 11,
+    "twelve": 12,
+    "thirteen": 13,
+    "fourteen": 14,
+    "fifteen": 15,
+    "sixteen": 16,
+    "seventeen": 17,
+    "eighteen": 18,
+    "nineteen": 19,
+    "twenty": 20,
+}
+
 with open(filename, newline="", encoding="utf-8") as f:
     reader = csv.DictReader(f)
     for row in reader:
